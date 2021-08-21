0662a46d26123137ebb90bc34013e0be66868768
Elbert <etse168@gmail.com>
2021-08-21 17:42:02 -0400

    Moved App.tsx out of src so expo start will work

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import App from '../src/App';
+import App from '../App';
 import renderer from 'react-test-renderer';
 
 it('renders the home screen', async () => {
