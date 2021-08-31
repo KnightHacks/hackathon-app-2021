8b1385422a15c7908679bf25c18492870d0fb730
Elbert <etse168@gmail.com>
2021-08-30 20:12:32 -0400

    Added dotenv

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { registerRootComponent } from 'expo';
 import React from 'react';
-import RootStack from './src/components/Navigation';
-
+import RootStack from './components/Navigation';
+// @ts-ignore
+// import {  } from 'react-native-dotenv';
 /**
  * The Main App.
  */
