5b85a90cdff5367fd63c6aad8468a4a00328ec51
Elbert <etse168@gmail.com>
2021-08-31 21:28:50 -0400

    Added Sentry init and echoing in workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { registerRootComponent } from 'expo';
 import React from 'react';
 import RootStack from './components/Navigation';
+import * as Sentry from 'sentry-expo';
 // @ts-ignore
-// import {  } from 'react-native-dotenv';
+import { SENTRY_DSN } from 'react-native-dotenv';
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  enableInExpoDevelopment: true,
+  debug: true,
+});
+
 /**
  * The Main App.
  */
