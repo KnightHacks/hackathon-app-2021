cbe0c44bb876629139c4f250996279fc40b387d8
timothyoei <timothy.oei@gmail.com>
2021-09-08 13:45:01 -0400

    Fixed useEffect crash

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -8,18 +8,21 @@ import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 dayjs.extend(duration);
 dayjs.extend(isSameOrBefore);
 
-const start = dayjs(new Date(2021, 11, 10, 16, 30, 0)), end = dayjs(new Date(2021, 11, 12, 16, 30, 0));
+const start = dayjs(new Date(2021, 11, 10, 16, 30, 0)), end = dayjs(new Date(2021, 11, 12, 16, 30, 0)),
+    clock = require('../../assets/clock.png');
 
 /* Countdown for end of hacking */
-export default function CountDown() {
+export default function CountDown() : JSX.Element {
     const [now, setNow] = useState(dayjs());
 
     useEffect(() => {
-        setTimeout(setNow(dayjs()), 1000)
+        setTimeout(() => {
+            setNow(dayjs());
+        }, 1000);
     });
 
     /* Renders the "time" part of the countdown */
-    function calcCountDown(time1, time2) {
+    function calcCountDown(time1 : dayjs.Dayjs, time2 : dayjs.Dayjs) {
         const duration = dayjs.duration(time2.diff(time1));
         if (duration.days() > 0)
             return `${duration.hours() + (24 * duration.days())}:${duration.format("mm:ss")}`;
@@ -29,19 +32,16 @@ export default function CountDown() {
     return (
         <View style={CountStyles.container}>
             <View style={CountStyles.card}>
-            <Image style={CountStyles.image} source={require('../../assets/clock.png')} />
+            <Image style={CountStyles.image} source={clock} />
                 <Text style={CountStyles.contents}>
-                    {
-                        (() => {
-                            if (now.isSameOrBefore(start)) {
-                                return `Hacking Starts In:\n${calcCountDown(now, start)}`;
-                            } else if (now.isSameOrBefore(end)) {
-                                return `Hacking Ends In:\n${calcCountDown(now, end)}`;
-                            } else {
-                                return "Hacking Has Ended";
-                            }
-                        })()
-                    }
+                    {(() => {
+                        if (now.isSameOrBefore(start))
+                            return `Hacking Starts In:\n${calcCountDown(now, start)}`;
+                        else if (now.isSameOrBefore(end))
+                            return `Hacking Ends In:\n${calcCountDown(now, end)}`;
+                        else 
+                            return "Hacking Has Ended";
+                    })()}
                 </Text>
             </View>
         </View>
