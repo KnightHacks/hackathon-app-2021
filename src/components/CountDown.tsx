3f314cf74545b40bbc660bcdb0d51dabd15703d8
timothyoei <timothy.oei@gmail.com>
2021-09-08 18:15:23 -0400

    Prettier test 3

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -8,43 +8,45 @@ import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 dayjs.extend(duration);
 dayjs.extend(isSameOrBefore);
 
-const start = dayjs(new Date(2021, 11, 10, 16, 30, 0)), end = dayjs(new Date(2021, 11, 12, 16, 30, 0)),
-    clock = require('../../assets/clock.png');
+const start = dayjs(new Date(2021, 11, 10, 16, 30, 0)),
+  end = dayjs(new Date(2021, 11, 12, 16, 30, 0)),
+  clock = require('../../assets/clock.png');
 
 /* Countdown for end of hacking */
-export default function CountDown() : JSX.Element {
-    const [now, setNow] = useState(dayjs());
+export default function CountDown(): JSX.Element {
+  const [now, setNow] = useState(dayjs());
 
-    /* Update the countdown */
-    useEffect(() => {
-        setTimeout(() => {
-            setNow(dayjs());
-        }, 1000);
-    });
+  /* Update the countdown */
+  useEffect(() => {
+    setTimeout(() => {
+      setNow(dayjs());
+    }, 1000);
+  });
 
-    return (
-        <View style={CountStyles.container}>
-            <View style={CountStyles.card}>
-            <Image style={CountStyles.image} source={clock} />
-                <Text style={CountStyles.contents}>
-                    {(() => {
-                        if (now.isSameOrBefore(start))
-                            return `Hacking Starts In:\n${diffHourMinSec(now, start)}`;
-                        else if (now.isSameOrBefore(end))
-                            return `Hacking Ends In:\n${diffHourMinSec(now, end)}`;
-                        else 
-                            return "Hacking Has Ended";
-                    })()}
-                </Text>
-            </View>
-        </View>
-    );
+  return (
+    <View style={CountStyles.container}>
+      <View style={CountStyles.card}>
+        <Image style={CountStyles.image} source={clock} />
+        <Text style={CountStyles.contents}>
+          {(() => {
+            if (now.isSameOrBefore(start))
+              return `Hacking Starts In:\n${diffHourMinSec(now, start)}`;
+            else if (now.isSameOrBefore(end))
+              return `Hacking Ends In:\n${diffHourMinSec(now, end)}`;
+            else return 'Hacking Has Ended';
+          })()}
+        </Text>
+      </View>
+    </View>
+  );
 }
 
 /* Renders the "time" part of the countdown */
-export function diffHourMinSec(time1 : dayjs.Dayjs, time2 : dayjs.Dayjs) {
-    const duration = dayjs.duration(time2.diff(time1));
-        if (duration.days() > 0 || duration.months() > 0 || duration.years() > 0)
-            return `${duration.hours() + (24 * time2.diff(time1, 'days'))}:${duration.format("mm:ss")}`;
-        return duration.format("HH:mm:ss");
+export function diffHourMinSec(time1: dayjs.Dayjs, time2: dayjs.Dayjs) {
+  const duration = dayjs.duration(time2.diff(time1));
+  if (duration.days() > 0 || duration.months() > 0 || duration.years() > 0)
+    return `${
+      duration.hours() + 24 * time2.diff(time1, 'days')
+    }:${duration.format('mm:ss')}`;
+  return duration.format('HH:mm:ss');
 }
