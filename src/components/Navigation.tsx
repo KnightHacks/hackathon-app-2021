c0afefd805c92c626e396c2b979d0302d10e8d74
suneettipirneni <suneettipirneni@icloud.com>
2021-09-11 12:58:25 -0400

    merge

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,6 +9,7 @@ import { AntDesign } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
 import Sponsors from '../screens/Sponsors';
 import Schedule from '../screens/Schedule';
+import { StatusBar } from 'expo-status-bar';
 // @ts-ignore
 import koipond from '../assets/KoiFishPond.png';
 
@@ -19,81 +20,83 @@ const Tab = createBottomTabNavigator();
  */
 function RootStack(): JSX.Element {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          headerTitleStyle: {
-            fontSize: 25,
-            fontWeight: '700',
-            alignSelf: 'flex-start',
-            color: 'white',
-          },
-          headerBackground: () => (
-            <ImageBackground
-              style={{
-                width: '100%',
-                height: '100%',
-                opacity: 50,
-              }}
-              source={koipond}
-            >
-              <View
+    <>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            headerTitleStyle: {
+              fontSize: 25,
+              fontWeight: '700',
+              alignSelf: 'flex-start',
+              color: 'white',
+            },
+            headerBackground: () => (
+              <ImageBackground
                 style={{
-                  backgroundColor: 'rgba(0, 0, 0, 0.40)',
                   width: '100%',
                   height: '100%',
                   opacity: 50,
                 }}
-              />
-            </ImageBackground>
-          ),
-          headerTitleAlign: 'left',
-          tabBarShowLabel: false,
-          tabBarIconStyle: {
-            height: 50,
-          },
-          tabBarStyle: {
-            backgroundColor: 'white',
-            alignContent: 'center',
-            position: 'absolute',
-            bottom: 25,
-            left: 45,
-            right: 45,
-            borderRadius: 45,
-            height: 60,
-            paddingBottom: 0,
-          },
-        }}
-      >
-        <Tab.Screen
-          name="Home"
-          component={Schedule}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <Feather name="home" size={24} color={color} />
+                source={koipond}
+              >
+                <View
+                  style={{
+                    backgroundColor: 'rgba(0, 0, 0, 0.40)',
+                    width: '100%',
+                    height: '100%',
+                    opacity: 50,
+                  }}
+                />
+              </ImageBackground>
             ),
-          }}
-        />
-        <Tab.Screen
-          name="About"
-          component={About}
-          options={{
-            tabBarIcon: ({ color }) => {
-              return <AntDesign name="book" size={24} color={color} />;
+            headerTitleAlign: 'left',
+            tabBarShowLabel: false,
+            tabBarIconStyle: {
+              height: 50,
+            },
+            tabBarStyle: {
+              alignContent: 'center',
+              position: 'absolute',
+              bottom: 25,
+              left: 45,
+              right: 45,
+              borderRadius: 45,
+              height: 60,
+              paddingBottom: 0,
             },
           }}
-        />
-        <Tab.Screen
-          name="Sponsors"
-          component={Sponsors}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <AntDesign name="team" size={24} color={color} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+        >
+          <Tab.Screen
+            name="Home"
+            component={Schedule}
+            options={{
+              tabBarIcon: ({ color }) => (
+                <Feather name="home" size={24} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="About"
+            component={About}
+            options={{
+              tabBarIcon: ({ color }) => {
+                return <AntDesign name="book" size={24} color={color} />;
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Sponsors"
+            component={Sponsors}
+            options={{
+              tabBarIcon: ({ color }) => (
+                <AntDesign name="team" size={24} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+      <StatusBar style="light" />
+    </>
   );
 }
 
