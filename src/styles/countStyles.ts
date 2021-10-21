57eaabc81ca8e683b795dab10a5cd6600dccb4f6
conroyr41 <conroyr41@gmail.com>
2021-10-21 17:25:55 -0400

    apply darkmode to countdown timer

--- src/styles/countStyles.ts
+++ src/styles/countStyles.ts
@@ -7,7 +7,7 @@ const Styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 10,
   },
-  card: {
+  cardLight: {
     width: '90%',
     display: 'flex',
     flexDirection: 'row',
@@ -21,13 +21,33 @@ const Styles = StyleSheet.create({
     shadowOpacity: 0.83,
     shadowRadius: 17,
   },
+  cardDark: {
+    width: '90%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#051a66',
+    padding: 10,
+    borderRadius: 10,
+    shadowColor: '#46C2FF',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.83,
+    shadowRadius: 17,
+  },
   contents: {
     color: 'white',
     fontWeight: 'bold',
     fontSize: 18,
   },
-  image: {
+  imageLight: {
     backgroundColor: '#46C2FF',
+    height: 40,
+    width: 40,
+    marginRight: 10,
+  },
+  imageDark: {
+    backgroundColor: '#051a66',
     height: 40,
     width: 40,
     marginRight: 10,
