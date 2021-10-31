4bfe08cf3d846e9cb0f2c3826c8454e828818b29
Elbert <etse168@gmail.com>
2021-10-31 17:24:31 -0400

    Removed caching

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,3 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { APIEvent } from '@knighthacks/hackathon';
 import { EventResponse } from './types';
 
@@ -7,32 +6,18 @@ const url: string = 'https://stagingapi.knighthacks.org/api';
 const api = {
   async getEvents(): Promise<APIEvent[]> {
     try {
-      const eventsList = await AsyncStorage.getItem('events');
-
       // Check if we had anything in async storage, if not, go to our endpoint
       // and get it and store it.
 
-      if (!eventsList) {
-        // eslint-disable-next-line
-        const response = await fetch(url + '/events/get_all_events/');
-        const { events, status }: EventResponse = await response.json();
+      // eslint-disable-next-line
+      const response = await fetch(url + '/events/get_all_events/');
+      const { events, status }: EventResponse = await response.json();
 
-        if (status !== 'success') {
-          // Unsuccessful, dont cache, return empty list
-          return [];
-        } else {
-          // Successful
-          if (events.length !== 0) {
-            // Event list isnt empty, cache
-            await AsyncStorage.setItem('events', JSON.stringify(events));
-          }
-          // Dont cache if empty list, can return as is if empty
-          return events;
-        }
+      if (status !== 'success') {
+        // Unsuccessful return empty list
+        return [];
       } else {
-        // Had events cached, return them
-        const cachedEvents = JSON.parse(eventsList);
-        return cachedEvents;
+        return events;
       }
     } catch (e) {
       throw new Error('Something went wrong trying to fetch events!');
