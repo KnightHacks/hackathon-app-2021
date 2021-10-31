87f841fe2c54adbbadc04a9a640197342fb7cff4
Timothy Oei <timothy.oei@gmail.com>
2021-10-31 17:04:39 -0400

    Renders sponsor cards, but still needs tests

--- src/api/api.ts
+++ src/api/api.ts
@@ -20,6 +20,29 @@ const api = {
       throw new Error('Something went wrong trying to fetch events!');
     }
   },
+  async getSponsors(): Promise<APISponsor[]> {
+    try {
+      const sponsorsList = await AsyncStorage.getItem('sponsors');
+
+      if (!sponsorsList) {
+        // eslint-disable-next-line
+        const response = await fetch(url + '/sponsors/get_all_events/');
+        const { sponsors, status }: SponsorResponse = await response.json();
+
+        if (status !== 'success') {
+          return [];
+        } else {
+          await AsyncStorage.setItem('sponsors', JSON.stringify(sponsors));
+          return sponsors;
+        }
+      } else {
+        const cachedSponsors = JSON.parse(sponsorsList);
+        return cachedSponsors;
+      }
+    } catch (e) {
+      throw new Error('Something went wrong trying to fetch sponsors!');
+    }
+  },
 };
 
 export default api;
--- src/api/types.d.ts
+++ src/api/types.d.ts
@@ -1,6 +1,11 @@
-import { APIEvent } from '@knighthacks/hackathon';
+import { APIEvent, APISponsor } from '@knighthacks/hackathon';
 
 export interface EventResponse {
   events: APIEvent[];
+  status: string;
+}
+
+export interface SponsorResponse {
+  sponsors: APISponsor[];
   status: string;
 }
