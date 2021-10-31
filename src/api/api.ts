1cc4d2e0a90c92eee7a93a43cc0bc034040831a6
Elbert <etse168@gmail.com>
2021-10-31 09:39:22 -0400

    CPulling from endpoint again

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,12 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { APIEvent } from '@knighthacks/hackathon';
-// import { EventResponse } from './types';
+import { EventResponse } from './types';
 
-// const url: string = 'https://stagingapi.knighthacks.org/api';
+const url: string = 'https://stagingapi.knighthacks.org/api';
 
 const api = {
   async getEvents(): Promise<APIEvent[]> {
     try {
-      await AsyncStorage.removeItem('events');
       const eventsList = await AsyncStorage.getItem('events');
 
       // Check if we had anything in async storage, if not, go to our endpoint
@@ -15,10 +14,8 @@ const api = {
 
       if (!eventsList) {
         // eslint-disable-next-line
-        // const response = await fetch(url + '/events/get_all_events/');
-        // const { events, status }: EventResponse = await response.json();
-        const status = 'success';
-        const events: APIEvent[] = [];
+        const response = await fetch(url + '/events/get_all_events/');
+        const { events, status }: EventResponse = await response.json();
 
         if (status !== 'success') {
           // Unsuccessful, dont cache, return empty list
@@ -44,5 +41,3 @@ const api = {
 };
 
 export default api;
-
-// TODO: Since we're testing with empty events, need to uncomment lines 3, 5, 18, 19 and delete lines 10, 20, 21
