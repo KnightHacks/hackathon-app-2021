13175f471e6a3687d2173c566f3f9419ae9fb323
Elbert <etse168@gmail.com>
2021-11-02 11:23:11 -0400

    Forgot to remove comment on caching

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,15 +6,12 @@ const url: string = 'https://stagingapi.knighthacks.org/api';
 const api = {
   async getEvents(): Promise<APIEvent[]> {
     try {
-      // Check if we had anything in async storage, if not, go to our endpoint
-      // and get it and store it.
-
       // eslint-disable-next-line
       const response = await fetch(url + '/events/get_all_events/');
       const { events, status }: EventResponse = await response.json();
 
       if (status !== 'success') {
-        // Unsuccessful return empty list
+        // Unsuccessful, return empty list
         return [];
       } else {
         return events;
