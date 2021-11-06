8eae406baec3a1f961959bc163cd26079e5fcb00
suneettipirneni <suneettipirneni@icloud.com>
2021-11-05 23:51:42 -0400

    add sponsor sorting

--- src/screens/Sponsors.tsx
+++ src/screens/Sponsors.tsx
@@ -5,6 +5,44 @@ import { ScrollView } from 'react-native-gesture-handler';
 import SponsorCard from '../components/cards/SponsorCard';
 import api from '../api/api';
 
+const comparator = (lhs: APISponsorData, rhs: APISponsorData) => {
+  if (lhs.subscription_tier === 'Gold') {
+    if (
+      rhs.subscription_tier === 'Silver' ||
+      rhs.subscription_tier === 'Bronze'
+    ) {
+      return -1;
+    }
+
+    return lhs.sponsor_name.localeCompare(rhs.sponsor_name);
+  }
+
+  if (lhs.subscription_tier === 'Silver') {
+    if (rhs.subscription_tier === 'Gold') {
+      return 1;
+    }
+
+    if (rhs.subscription_tier === 'Bronze') {
+      return -1;
+    }
+
+    return lhs.sponsor_name.localeCompare(rhs.sponsor_name);
+  }
+
+  if (lhs.subscription_tier === 'Bronze') {
+    if (
+      rhs.subscription_tier === 'Gold' ||
+      rhs.subscription_tier === 'Silver'
+    ) {
+      return 1;
+    }
+
+    return lhs.sponsor_name.localeCompare(rhs.sponsor_name);
+  }
+
+  return 0;
+};
+
 /**
  * The Sponsors page for knight hacks.
  */
@@ -14,7 +52,8 @@ function Sponsors(): JSX.Element {
   useEffect(() => {
     async function getSponsors() {
       const res = await api.getSponsors();
-      setSponsors(res);
+      console.log(res);
+      setSponsors(res.sort(comparator));
     }
     getSponsors();
   }, []);
