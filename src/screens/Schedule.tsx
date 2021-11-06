4b05729e936bf6cb8cbcf6db45d3622227b7d897
Elbert <etse168@gmail.com>
2021-11-06 10:44:20 -0400

    Sorting events by start time

--- src/screens/Schedule.tsx
+++ src/screens/Schedule.tsx
@@ -6,10 +6,30 @@ import { APIEventData } from '@knighthacks/hackathon';
 import SearchBar from '../components/SearchBar';
 import ErrorMsgContainer from '../components/ErrorMsgContainer';
 import api from '../api/api';
+import dayjs from 'dayjs';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+dayjs.extend(isSameOrBefore);
 
 if (Platform.OS === 'android') {
   if (UIManager.setLayoutAnimationEnabledExperimental) {
     UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+}
+
+function comparator(lhs: APIEventData, rhs: APIEventData) {
+  // Same start time, check end
+  if (dayjs(lhs.date_time).isSame(dayjs(rhs.date_time))) {
+    if (dayjs(lhs.end_date_time).isSameOrBefore(dayjs(rhs.end_date_time))) {
+      return -1;
+    } else {
+      return 1;
+    }
+  } else {
+    if (dayjs(lhs.date_time).isBefore(dayjs(rhs.date_time))) {
+      return -1;
+    } else {
+      return 1;
+    }
   }
 }
 
@@ -26,8 +46,9 @@ function Schedule(): JSX.Element {
   useEffect(() => {
     async function getEvents() {
       const res = await api.getEvents();
-      setEvents(res);
-      setCurEvents(res);
+      const sorted = res.sort(comparator);
+      setEvents(sorted);
+      setCurEvents(sorted);
     }
     getEvents();
   }, []);
